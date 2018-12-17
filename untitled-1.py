bc62497594c515e9574dd56e03f1fbc73841f558
AngryPythonCoder <45202735+AngryPythonCoder@users.noreply.github.com>
2018-12-18 00:17:31 +0500

    Add files via upload

--- untitled-1.py
+++ untitled-1.py
@@ -14,7 +14,6 @@ def refresh(path):
         for obj in os.listdir(path):
             if obj.split('.')[-1] == 'mp3':
                 list_of_music.append(obj)
-                print(obj)
     return list_of_music
 
 
@@ -22,7 +21,6 @@ class MyWidget(QMainWindow,Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
         self.player = pyglet.media.Player()      
         self.current_pos = 0
         self.rewind_slider = 0
@@ -33,34 +31,31 @@ class MyWidget(QMainWindow,Ui_MainWindow):
                 self.music_path = json.load(input_file)
         except FileNotFoundError:
             self.music_path = None
-        
-        
         self.list_of_music = refresh(self.music_path)
-        print(self.list_of_music)
-        
         self.VolumeSlider.setValue(10)
-        
         self.MinusButton.clicked.connect(self.minus)
         self.PauseButton.clicked.connect(self.pause)
         self.PlayButton.clicked.connect(self.play)
         self.PlusButton.clicked.connect(self.plus)
-        
         self.MusicSlider.valueChanged.connect(self.rewind)
         self.VolumeSlider.valueChanged.connect(self.change_volume)
-        self.RewindSlider.valueChanged.connect(self.rewind_control)
+        self.RewindSlider.valueChanged.connect(self.rewind_control)           
  
     def change_volume(self, number):
         self.player.volume = number / 10    
         
     def control_time(self):
-        while current_pos < length:
-            if active:
-                current_pos = player.time
-                self.CurrentTimeBrowser.setText(str(int(current_pos) // 60) + ':' + str(int(current_pos) % 60).zfill(2))
-            time.sleep(1)    
+        while self.status == 'Empty':
+            time.sleep(1)
+        print(1)
+        while self.current_pos < self.length:
+            if self.status == 'Full':
+                self.current_pos = self.player.time
+                self.CurrentTimeBrowser.setText(str(int(self.current_pos) // 60) + ':' + str(int(self.current_pos) % 60).zfill(2))
+            time.sleep(1)
         
     def minus(self):
-        shift = max(0, player.time - ARRAY[special_slider])
+        shift = max(0, self.player.time - ARRAY[self.rewind_slider])
         self.player.seek(shift)
         self.MusicSlider.setValue(shift)   
         
@@ -70,11 +65,12 @@ class MyWidget(QMainWindow,Ui_MainWindow):
             self.length = music.duration
             self.player.queue(music)
             self.MusicSlider.setMaximum(self.length)
-            self.MusicLengthLabel.setText('{}:{}'.format(length // 60, str(length % 60).zfill(2)))
-        self.player.play()
+            self.MusicLengthLabel.setText(str(int(self.length) // 60) + ':' + str(int(self.length) % 60).zfill(2))
+            self.status = 'Full'
+        self.player.play()     
         
     def plus(self):
-        shift = min(length, player.time + ARRAY[special_slider])
+        shift = min(self.length, self.player.time + ARRAY[self.rewind_slider])
         self.player.seek(shift)
         self.MusicSlider.setValue(shift)    
         
@@ -92,4 +88,9 @@ ARRAY = [5, 15, 60, 300]
 app = QApplication(sys.argv)
 ex = MyWidget()
 ex.show()
-sys.exit(app.exec_()) 
+t1 = Thread(target=app.exec_)
+t2 = Thread(target=ex.control_time)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
